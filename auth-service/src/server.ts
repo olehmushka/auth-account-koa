0a12b727d347a2e554c0d7b32d08d6f8b79d3eeb
Oleh Mushka <olegamysk@gmail.com>
2020-03-10 11:31:56 +0200

    added catching errors to koa-oas3 usage

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,8 +1,9 @@
-import koa from 'koa';
+import koa, { Context } from 'koa';
 import koaBodyparser from 'koa-bodyparser';
 import koaJson from 'koa-json';
 import koaLogger from 'koa-logger';
 import * as koaOas3 from 'koa-oas3';
+import compose from 'koa-compose';
 import { app } from './app/app';
 
 const server = new koa();
@@ -11,11 +12,30 @@ server.use(koaBodyparser());
 server.use(koaJson());
 server.use(koaLogger());
 
-server.use(koaOas3.oas({
-  file: `${__dirname}/../k8s/openapi.yml`,
-  endpoint: '/openapi.json',
-  uiEndpoint: '/'
-}))
+server.use(compose([
+  async(ctx: Context, next: () => Promise<any>): Promise<void> => {
+    try {
+      await next();
+    } catch (e) {
+      ctx.status = 400;
+      ctx.body = {
+        code: 400,
+        message: 'Request validation failed',
+        e: e,
+        details: {
+          where: e.where,
+          name: e.key,
+          message: e.message,
+        },
+      };
+    }
+  },
+  koaOas3.oas({
+    file: `${__dirname}/../k8s/openapi.yml`,
+    endpoint: '/openapi.json',
+    uiEndpoint: '/'
+  }),
+]))
 
 server.use(app.routes());
 server.use(app.allowedMethods());
